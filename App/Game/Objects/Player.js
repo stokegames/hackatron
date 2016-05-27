69737c0a088c463329aad768ce303e11d7c56efe
Eric Muyser <eric@muyser.com>
2016-05-26 21:38:26 -0700

    Player should use ES6 class

--- App/Game/Objects/Player.js
+++ App/Game/Objects/Player.js
@@ -1,84 +1,82 @@
 import GameObject from '../Core/GameObject';
 import Tron from '../Objects/Tron';
 
-var Player = function() {
-};
-
-Player.prototype.toString = function() {
-    return `[Player x=${this.position.x} y=${this.position.y}]`;
-};
-
-Player.prototype.init = function(params) {
-    this.character = new Tron(params);
-    this.character.init(params);
-
-    this.id = params.id;
-    this.game = params.game;
-    this.name = params.name;
-    this.keys = params.keys;
-    this.points = params.points || 0;
-
-    this.setupKeys();
-};
-
-// Method for registering hardware keys to a given sprite
-Player.prototype.setupKeys = function() {
-    if (!this.keys) { return; }
-
-    if (this.keys.up) { this.character.sprite.upKey = this.game.input.keyboard.addKey(this.keys.up); }
-    if (this.keys.down) { this.character.sprite.downKey = this.game.input.keyboard.addKey(this.keys.down); }
-    if (this.keys.left) { this.character.sprite.leftKey = this.game.input.keyboard.addKey(this.keys.left); }
-    if (this.keys.right) { this.character.sprite.rightKey = this.game.input.keyboard.addKey(this.keys.right); }
-
-    // register attack key if it exists
-    if (this.keys.att) {
-        this.character.sprite.attKey = this.game.input.keyboard.addKey(this.keys.att);
-        this.character.sprite.attKey.onDown.add(this.character.triggerAttack, this.character);
+class Player {
+    toString() {
+        return `[Player x=${this.position.x} y=${this.position.y}]`;
     }
-};
 
-Player.prototype.removePoints = function(points) {
-    this.points -= points;
+    init(params) {
+        this.character = new Tron(params);
+        this.character.init(params);
 
-    if (this.points < 0) {
-        this.points = 0;
+        this.id = params.id;
+        this.game = params.game;
+        this.name = params.name;
+        this.keys = params.keys;
+        this.points = params.points || 0;
+
+        this.setupKeys();
     }
-};
 
-Player.prototype.addPoints = function(points) {
-    this.points += points;
-};
+    // Method for registering hardware keys to a given sprite
+    setupKeys() {
+        if (!this.keys) { return; }
 
-Player.prototype.hide = function() {
-    this.character.sprite.alpha = 0;
-};
+        if (this.keys.up) { this.character.sprite.upKey = this.game.input.keyboard.addKey(this.keys.up); }
+        if (this.keys.down) { this.character.sprite.downKey = this.game.input.keyboard.addKey(this.keys.down); }
+        if (this.keys.left) { this.character.sprite.leftKey = this.game.input.keyboard.addKey(this.keys.left); }
+        if (this.keys.right) { this.character.sprite.rightKey = this.game.input.keyboard.addKey(this.keys.right); }
 
-Player.prototype.show = function() {
-    this.character.sprite.alpha = 1;
-};
-
-Player.prototype.kill = function() {
-    this.character.kill();
-};
-
-Player.prototype.destroy = function() {
-    this.nameText.destroy();
-
-    if (this.keys) {
-        this.keys.up && this.game.input.keyboard.removeKey(this.keys.up);
-        this.keys.down && this.game.input.keyboard.removeKey(this.keys.down);
-        this.keys.left && this.game.input.keyboard.removeKey(this.keys.left);
-        this.keys.right && this.game.input.keyboard.removeKey(this.keys.right);
-
-        this.keys.att && this.game.input.keyboard.removeKey(this.keys.att);
+        // register attack key if it exists
+        if (this.keys.att) {
+            this.character.sprite.attKey = this.game.input.keyboard.addKey(this.keys.att);
+            this.character.sprite.attKey.onDown.add(this.character.triggerAttack, this.character);
+        }
     }
-};
 
-Object.defineProperty(Player.prototype, 'name', {
-    get: function() {
+    removePoints(points) {
+        this.points -= points;
+
+        if (this.points < 0) {
+            this.points = 0;
+        }
+    }
+
+    addPoints(points) {
+        this.points += points;
+    }
+
+    hide() {
+        this.character.sprite.alpha = 0;
+    }
+
+    show() {
+        this.character.sprite.alpha = 1;
+    }
+
+    kill() {
+        this.character.kill();
+    }
+
+    destroy() {
+        this.nameText.destroy();
+
+        if (this.keys) {
+            this.keys.up && this.game.input.keyboard.removeKey(this.keys.up);
+            this.keys.down && this.game.input.keyboard.removeKey(this.keys.down);
+            this.keys.left && this.game.input.keyboard.removeKey(this.keys.left);
+            this.keys.right && this.game.input.keyboard.removeKey(this.keys.right);
+
+            this.keys.att && this.game.input.keyboard.removeKey(this.keys.att);
+        }
+    }
+
+    get name() {
         return this._name;
-    },
-    set: function(name) {
+    }
+
+    set name(name) {
         if (!name) {
             name = this.id.substring(0, 2);
         }
@@ -96,7 +94,7 @@ Object.defineProperty(Player.prototype, 'name', {
 
         this.character.sprite.addChild(this.nameText);
     }
-});
+}
 
 // Wow, we may want this for "logic scripts"
 //
