2657ae8fbe9764c3d60a27b5a0cb561e961956e3
Eric Muyser <eric@muyser.com>
2016-05-26 21:40:53 -0700

    Gameover should use ES6 class

--- App/Game/Objects/Gameover.js
+++ App/Game/Objects/Gameover.js
@@ -1,22 +1,21 @@
-var Gameover = function() {
-};
+class Gameover {
+    init(game) {
+        this.game = game;
+        this.sprite = null;
+    }
 
-Gameover.prototype.init = function(game) {
-    this.game = game;
-    this.sprite = null;
-};
+    start() {
+        var self = this;
+        var tween = null;
+        var centerX = self.game.camera.width / 2;
+        var centerY = self.game.camera.height / 2;
 
-Gameover.prototype.start = function() {
-    var self = this;
-    var tween = null;
-    var centerX = self.game.camera.width / 2;
-    var centerY = self.game.camera.height / 2;
-
-    // Gameover
-    this.sprite = self.game.add.sprite(centerX, centerY, 'gfx/overlays/gameover');
-    //count.body.moves = false;
-    this.sprite.anchor.setTo(0.5);
-    tween = self.game.add.tween(this.sprite.scale).to({ x: 2.5, y: 2.5}, 600, Phaser.Easing.Exponential.In, true);
-};
+        // Gameover
+        this.sprite = self.game.add.sprite(centerX, centerY, 'gfx/overlays/gameover');
+        //count.body.moves = false;
+        this.sprite.anchor.setTo(0.5);
+        tween = self.game.add.tween(this.sprite.scale).to({ x: 2.5, y: 2.5}, 600, Phaser.Easing.Exponential.In, true);
+    }
+}
 
 export default Gameover;
