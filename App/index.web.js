b66fbd5a45337d9cad5d6caf5a174c0b96f4c217
Eric Muyser <eric@muyser.com>
2016-07-09 15:17:38 -0700

    Delinting

--- App/index.web.js
+++ App/index.web.js
@@ -44,8 +44,8 @@ class App extends Component {
 ////////////// PHASER GAME
 
 window.Hackatron = {}
-Hackatron.ready = false
-Hackatron.screen = 'Menu'
+window.Hackatron.ready = false
+window.Hackatron.screen = 'Menu'
 
 require('./Game/States/Boot')
 require('./Game/States/Preload')
@@ -53,56 +53,56 @@ require('./Game/States/Menu')
 require('./Game/States/Game')
 
 if (Utils.env.os.mobile) {
-    Hackatron.GAME_WIDTH = 256 // Game originally designed for 512px
-    Hackatron.GAME_HEIGHT = 256 * (window.innerHeight / window.innerWidth) // Game originally designed for 512px
+    window.Hackatron.GAME_WIDTH = 256 // Game originally designed for 512px
+    window.Hackatron.GAME_HEIGHT = 256 * (window.innerHeight / window.innerWidth) // Game originally designed for 512px
 } else {
-    Hackatron.GAME_WIDTH = 640 // Game originally designed for 640px
-    Hackatron.GAME_HEIGHT = 640 // Game originally designed for 640px
+    window.Hackatron.GAME_WIDTH = 640 // Game originally designed for 640px
+    window.Hackatron.GAME_HEIGHT = 640 // Game originally designed for 640px
 }
 
 // Game Constants
-Hackatron.UI_WIDTH = Hackatron.GAME_WIDTH // UI originally designed for 700px
-Hackatron.UI_HEIGHT = Hackatron.GAME_HEIGHT // UI originally designed for 700px
-Hackatron.TILE_COUNT_HORIZONTAL = 40
-Hackatron.TILE_COUNT_VERTICAL = 40
-Hackatron.GAME_TIME = 5 * 60
-Hackatron.DEFAULT_PLAYER_SPEED = 200
-Hackatron.POWERUP_SPAWN_INTERVAL = 5000
-Hackatron.UPDATE_INTERVAL = 100
+window.Hackatron.UI_WIDTH = window.Hackatron.GAME_WIDTH // UI originally designed for 700px
+window.Hackatron.UI_HEIGHT = window.Hackatron.GAME_HEIGHT // UI originally designed for 700px
+window.Hackatron.TILE_COUNT_HORIZONTAL = 40
+window.Hackatron.TILE_COUNT_VERTICAL = 40
+window.Hackatron.GAME_TIME = 5 * 60
+window.Hackatron.DEFAULT_PLAYER_SPEED = 200
+window.Hackatron.POWERUP_SPAWN_INTERVAL = 5000
+window.Hackatron.UPDATE_INTERVAL = 100
 
 var gameContainer = document.getElementById('game')
 var uiContainer = document.getElementById('ui')
 
-Hackatron.getWidthGameScale = function() {
-    return (window.innerWidth / Hackatron.GAME_WIDTH).toFixed(2)
+window.Hackatron.getWidthGameScale = function() {
+    return (window.innerWidth / window.Hackatron.GAME_WIDTH).toFixed(2)
 }
 
-Hackatron.getHeightGameScale = function() {
-    return (window.innerHeight / Hackatron.GAME_HEIGHT).toFixed(2)
+window.Hackatron.getHeightGameScale = function() {
+    return (window.innerHeight / window.Hackatron.GAME_HEIGHT).toFixed(2)
 }
 
-Hackatron.getWidthRatioScale = function() {
+window.Hackatron.getWidthRatioScale = function() {
     return window.innerHeight > window.innerWidth ? 1 : (window.innerHeight / window.innerWidth).toFixed(2)
 }
 
-Hackatron.getHeightRatioScale = function() {
+window.Hackatron.getHeightRatioScale = function() {
     return window.innerHeight > window.innerWidth ? (window.innerWidth / window.innerHeight).toFixed(2) : 1
 }
 
 // Resize UI
 window.onresize = function() {
-    var smallest = window.innerWidth < window.innerHeight ? window.innerWidth / Hackatron.UI_WIDTH : window.innerHeight / Hackatron.UI_HEIGHT
+    var smallest = window.innerWidth < window.innerHeight ? window.innerWidth / window.Hackatron.UI_WIDTH : window.innerHeight / window.Hackatron.UI_HEIGHT
     uiContainer.style.zoom = smallest
 }
 
 // Load Game
-Hackatron.debug = window.location.href.indexOf('localhost') !== -1
-Hackatron.loader = new Phaser.Game(Hackatron.GAME_WIDTH, Hackatron.GAME_HEIGHT, Phaser.AUTO, gameContainer, null, true)
+window.Hackatron.debug = window.location.href.indexOf('localhost') !== -1
+window.Hackatron.loader = new Phaser.Game(window.Hackatron.GAME_WIDTH, window.Hackatron.GAME_HEIGHT, Phaser.AUTO, gameContainer, null, true)
 
-Hackatron.loader.state.add('Boot', Hackatron.Boot)
-Hackatron.loader.state.add('Preload', Hackatron.Preload)
-Hackatron.loader.state.add('Menu', Hackatron.Menu)
-Hackatron.loader.state.add('Game', Hackatron.Game)
+window.Hackatron.loader.state.add('Boot', window.Hackatron.Boot)
+window.Hackatron.loader.state.add('Preload', window.Hackatron.Preload)
+window.Hackatron.loader.state.add('Menu', window.Hackatron.Menu)
+window.Hackatron.loader.state.add('Game', window.Hackatron.Game)
 
 window.onresize()
 
@@ -128,4 +128,3 @@ window.Hackatron.fitToWindow = function() {
 
 
 ReactDOM.render(<App />, document.getElementById('ui'))
-
